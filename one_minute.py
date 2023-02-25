2ff43848f76d6eb0ab25ba7b7c5b5271a7098191
KristinEbu <103155833+KristinEbu@users.noreply.github.com>
2023-02-24 16:21:04 -0800

    formatting changes

--- one_minute.py
+++ one_minute.py
@@ -2,15 +2,24 @@ import serial
 import time
 import sys
 
+def format_output(s):
+    s = s.replace("'", "")
+    s = s.replace("b", "")
+    s = s.replace("\\r\\n", "\n")
+    return s
+
 def one_minute():
     ser = serial.Serial('/dev/ttyUSB0', 115200)  # open serial port
     currentTime = time.monotonic()
-    finalTime = currentTime + 60
+    finalTime = currentTime + 60 # one minute
     fileName = sys.argv[1]
     f = open(fileName, "x")
+    
     while(currentTime < finalTime):
         x = ser.read_until(expected=b"\r\n")
+        s = format_output(str(x))
         f.write(x)
+        currentTime = time.monotonic()
     f.close()
     ser.close()
 
