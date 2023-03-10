a283eb97bbc705bfd00a2866fb0b2e66f4bfe8b4
KristinEbu <103155833+KristinEbu@users.noreply.github.com>
2023-03-10 15:12:14 -0800

    removed blank lines

--- one_minute.py
+++ one_minute.py
@@ -13,14 +13,12 @@ def one_minute():
     #ser = serial.Serial('COM6', 115200) # open serial port Windows
     initialTime = time.monotonic()
     currentTime = time.monotonic()
-
     finalTime = currentTime + 60		# one minute
     fileName = sys.argv[1]
     f = open(fileName, "x")
     
     while(currentTime < finalTime):
         x = ser.read_until(expected=b"\r\n")
-        
         s = format_output(str(x))
         currentTime = time.monotonic()
         f.write(s + "time elapsed = " + str(currentTime - initialTime) + "\n")
